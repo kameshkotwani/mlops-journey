bda5edf73a5c88197e8d5ea20b795d7535944706
Kamesh Kotwani <kameshkotwani@gmail.com>
2024-01-23 22:05:07 +0000

    created a train experiment

---
+++ src/train.py
@@ -0,0 +1,16 @@
+import random
+import sys
+from dvclive import Live
+import yaml
+
+
+with Live(save_dvc_exp=True) as live:
+    train_params = yaml.safe_load(open("src\params\params.yaml"))['train']
+    epochs = train_params['epochs']
+    live.log_param("epochs", epochs)
+    for epoch in range(epochs):
+        live.log_metric("train/accuracy", epoch + random.random())
+        live.log_metric("train/loss", epochs - epoch - random.random())
+        live.log_metric("val/accuracy",epoch + random.random() )
+        live.log_metric("val/loss", epochs - epoch - random.random())
+        live.next_step()
